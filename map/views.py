c089f45fc65aa0ffbdd892476545a5f40e3fac9a
Chengran Jin <cj2671@columbia.edu>
2021-04-11 08:51:00 +0000

    Map Views/Urls

--- map/views.py
+++ map/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Squirrel
 
-# Create your views here.
+def index(request):
+    squirrel_map = Squirrel.objects.all()[:100]
+    context = {
+            'squirrel_map':  Squirrel.objects.all()[:100]
+    }
+
+    return render(request, 'map/index.html', context)
+
+
--- sightings/apps.py
+++ sightings/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
 
-class SightingsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+class SightingsConfig(AppConfig):'
     name = 'sightings'
