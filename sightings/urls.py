ae3fcffa18a7c2cda363e617336639283b35de32
zhangwhbilly <wz2451@columbia.edu>
2021-04-14 15:44:17 +0000

    Modified urls.py

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name = "home"),
     path('sightings/', views.sightings, name = "sightings"),
-    path('sightings/<unique_squirrel_id>/', views.update_sighting, name = "update"),
+    # path('sightings/<unique_squirrel_id>/', views.update_sighting, name = "update"),
     path('sightings/add/', views.create_sighting, name = "add"),
     path('sightings/stats/', views.stats, name = "stats"),
+    path('sightings/<unique_squirrel_id>/', views.update_sighting, name = "update"),
 ]
