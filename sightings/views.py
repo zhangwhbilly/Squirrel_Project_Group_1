f024a73b4cb78151a120e4bc3c5dd8f42b8b6244
zhangwhbilly <wz2451@columbia.edu>
2021-04-14 11:51:23 +0000

    Modified views.py Stage 1

--- sightings/views.py
+++ sightings/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from map.models import Squirrel
 
-# Create your views here.
+
+def sightings():
+    Squirrels = Squirrel.objects.all()
+    context = {
+        'Squirrels': Squirrels,
+    }
+    return render(request, 'sightings/', context)
+
+def update_sighting(request):
+    squirrel_detail = get_object_or_404(Squirrel, unique_squirrel_id)
+    if request.method == 'POST':
+        form = SightingsForm(request.POST, instance = squirrel_detail)
+        if form.is_valid():
+            form.save()
+            return redirect(f'sightings/{unique_squirrel_id}')
+        else:
+            form = SightingsForm(instance = squirrel_detail)
+    context = {
+        'form': form,
+    }
+    return render(request, 'sightings/update.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def create_sighting(request):
+    if request.method = 'POST':
+        form = SightingsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/sightings/")
+        else:
+            form = SightingsForm()
+            context = {
+                'form': form,
+            }
+    return render(request, 'sightings/add.html', context)
+
