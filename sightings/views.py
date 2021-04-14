1cb6c0db28056f41dfe0f3b4aa94ab2a966c9339
zhangwhbilly <wz2451@columbia.edu>
2021-04-14 12:48:03 +0000

    Modified views.py Stage 2

--- sightings/views.py
+++ sightings/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 from map.models import Squirrel
 
 
+
 def sightings():
     Squirrels = Squirrel.objects.all()
     context = {
         'Squirrels': Squirrels,
     }
     return render(request, 'sightings/', context)
+
 
 def update_sighting(request):
     squirrel_detail = get_object_or_404(Squirrel, unique_squirrel_id)
@@ -24,30 +26,36 @@ def update_sighting(request):
     return render(request, 'sightings/update.html', context)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def create_sighting(request):
     if request.method = 'POST':
         form = SightingsForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("/sightings/")
+            return redirect("sightings/")
         else:
             form = SightingsForm()
-            context = {
-                'form': form,
-            }
+    context = {
+        'form': form,
+    }
     return render(request, 'sightings/add.html', context)
+
+
+def stats(request):
+    total = Squirrel.objects.all().count()
+    gray_count = Squirrel.objects.filter(primary_fur_color = 'Gray').count()
+    PM_count = Squirrel.objects.filter(shift = 'PM').count()
+    AM_count = Squirrel.objects.filter(shift = 'AM').count()
+    avg_lat = Squirrel.objects.aggregate(Avg('latitude'))
+    avg_long = Squirrel.objects.aggregate(Avg('longitude'))
+    above_count = Squirrel.objects.filter(location = 'Above Ground').count()
+    context = {
+        'total': total,
+        'gray_count': gray_count,
+        'PM_count': PM_count,
+        'AM_count': AM_count,
+        'avg_lat': avg_lat,
+        'avg_long': avg_long,
+        'above_count': above_count,
+    }
+    return render(request, 'sightings/stats.html', context)
 
